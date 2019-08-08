6ba5a712ab4599b65b7758f633ff52741ed44f4c
pris0213 <priscila.vivian@acad.pucrs.br>
2019-08-08 19:15:36 -0300

    Iniciando Models

---
+++ nuvemmusicalapp/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+import datetime
+
+
+# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=500)
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=1000)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    release_date = models.DateTimeField('date of release')
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=1000)
+    duration = models.FloatField(default=0)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def duration_to_string(self):
+
+        duration = self.floatfield_to_float(self.duration)
+
+        try:
+            if duration < 0:
+                raise ValueError
+            if duration >= 0:
+                return str(datetime.timedelta(seconds=duration))
+        except ValueError:
+            print("The duration of the song can\'t be negative.")
+
+
+    def floatfield_to_float(self, a_floatfield_var):
+        var_as_string = str(a_floatfield_var)
+        return float(var_as_string)
---
+++ nuvemmusicalapp/tests/test_models.py
@@ -0,0 +1,39 @@
+import datetime
+from django.test import TestCase
+from ..models import Album, Artist, Song
+
+
+class SongModelTest(TestCase):
+
+    def create_song(self, title, duration, artist_name, album_name, album_release_date):
+        artist = Artist.objects.create(name=artist_name)
+
+        album = Album.objects.create(name=album_name,
+                                     artist=artist,
+                                     release_date=album_release_date)
+
+        return Song.objects.create(title=title,
+                                  duration=duration,
+                                  artist=artist,
+                                  album=album)
+
+    def test_duration_to_string_with_positive_number(self):
+        fav_song = self.create_song("Ghost Ship",
+                                    299,
+                                    "Blur",
+                                    "The Magic Whip",
+                                    datetime.date(2015, 4, 27))
+
+        self.assertEqual(fav_song.duration_to_string(), "0:04:59")
+
+    def test_duration_to_string_with_negative_number(self):
+        fav_song = self.create_song("Ghost Ship",
+                                    -1,
+                                    "Blur",
+                                    "The Magic Whip",
+                                    datetime.date(2015, 4, 27))
+
+        self.assertRaises(ValueError)
+
+
+
